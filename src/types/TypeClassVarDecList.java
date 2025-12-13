689f11635380e5a0bf916442c75046d978c7c3ad
omridavidi <omridvd@gmail.com>
2025-12-13 15:56:42 +0200

    Add classVarDecListFromAst method to construct TypeClassVarDecList from AstcFieldList

--- src/types/TypeClassVarDecList.java
+++ src/types/TypeClassVarDecList.java
@@ -1,4 +1,5 @@
 package types;
+import ast.*;
 
 public class TypeClassVarDecList
 {
@@ -33,5 +34,51 @@ public class TypeClassVarDecList
 		return this.tail.findElement(name);
 	}
 	return null;
+	}
+	
+	public static TypeClassVarDecList classVarDecListFromAst(AstcFieldList cFieldList) {
+		if (cFieldList == null) {
+			return null;
+		}
+		
+		AstDec dec = cFieldList.head.dec;
+		String fieldName = null;
+		Type fieldType = null;
+		
+		// Extract name and type based on declaration type
+		if (dec instanceof AstVarDec)
+			{
+			// if dec is a variable declaration
+			AstVarDec vd = (AstVarDec) dec;
+			fieldName = vd.name;
+			fieldType = vd.type.semantMe();
+			}
+		else if (dec instanceof AstFuncDec)
+			{
+			// if dec is a function declaration
+
+			AstFuncDec fd = (AstFuncDec) dec;
+			fieldName = fd.name;
+
+			// Build function type from signature
+			Type returnType = fd.type.semantMe();		
+			TypeList params = (fd.paramList != null ? fd.paramList.buildTypeList() : null);
+
+			fieldType = new TypeFunction(returnType, fd.name, params);
+		}
+		
+		else
+		{
+			throw new RuntimeException("Unknown declaration type in class field list");
+		}
+
+		
+		// Create the current node
+		TypeClassVarDec varDec = new TypeClassVarDec(fieldType, fieldName);
+		
+		// Recursively process the tail
+		TypeClassVarDecList tail = classVarDecListFromAst(cFieldList.tail);
+		
+		return new TypeClassVarDecList(varDec, tail);
 	}
 }
